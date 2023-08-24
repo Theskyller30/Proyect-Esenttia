3b8ec1b61e7916dd3adf5b4067e884974e299969
Juan Garcia <juangarcia123457854@gmail.com>
2023-08-24 01:02:24 -0500

    Version 2.5 Muestra

--- src/app/Components/layout/layout.module.ts
+++ src/app/Components/layout/layout.module.ts
@@ -4,18 +4,19 @@ import { CommonModule } from '@angular/common';
 import { LayoutRoutingModule } from './layout-routing.module';
 import { ReporteComponent } from './Pages/reporte/reporte.component';
 import { UsuarioComponent } from './Pages/usuario/usuario.component';
-import { HistorialVentaComponent } from './Pages/historial-venta/historial-venta.component';
+
 import { DashBoardComponent } from './Pages/dash-board/dash-board.component';
 import { ProductoComponent } from './Pages/producto/producto.component';
 import { VentaComponent } from './Pages/venta/venta.component';
 import { SharedModule } from 'src/app/Reusable/shared/shared.module';
+import { HistorialVentaComponent } from './Pages/historial-venta/historial-venta.component';
 
 
 @NgModule({
   declarations: [
     ReporteComponent,
     UsuarioComponent,
-    HistorialVentaComponent,
+HistorialVentaComponent,
     DashBoardComponent,
     ProductoComponent,
     VentaComponent
